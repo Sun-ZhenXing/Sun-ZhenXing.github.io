34ed57774c8036ea3048a0e57c0f510311535596
Sun-ZhenXing <1006925066@qq.com>
2022-12-04 15:32:00 +0800

    update: icons

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -2,5 +2,5 @@ import { navbar } from "vuepress-theme-hope";
 
 export const Navbar = navbar([
   "/",
-  { text: "演示", icon: "discover", link: "/demo/" }
+  // { text: "演示", icon: "discover", link: "/demo/" }
 ]);
