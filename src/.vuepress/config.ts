75214cf55ffe62ebbae0451f86c9083f3d9cb3b9
Sun-ZhenXing <1006925066@qq.com>
2023-02-20 16:02:10 +0800

    feat: add docsearch

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from 'vuepress'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import theme from './theme.js'
 
 export default defineUserConfig({
@@ -19,6 +20,53 @@ export default defineUserConfig({
       lineNumbers: 10,
     }
   },
+
+  plugins: [
+    docsearchPlugin({
+      appId: 'DF0MWQNCKW',
+      apiKey: 'fde5c0f2e0052f31234eb16cbb3dfcc3',
+      indexName: 'alexsun',
+      placeholder: '搜索文档',
+      translations: {
+        button: {
+          buttonText: '搜索文档',
+          buttonAriaLabel: '搜索文档',
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: '清除查询条件',
+            resetButtonAriaLabel: '清除查询条件',
+            cancelButtonText: '取消',
+            cancelButtonAriaLabel: '取消',
+          },
+          startScreen: {
+            recentSearchesTitle: '搜索历史',
+            noRecentSearchesText: '没有搜索历史',
+            saveRecentSearchButtonTitle: '保存至搜索历史',
+            removeRecentSearchButtonTitle: '从搜索历史中移除',
+            favoriteSearchesTitle: '收藏',
+            removeFavoriteSearchButtonTitle: '从收藏中移除',
+          },
+          errorScreen: {
+            titleText: '无法获取结果',
+            helpText: '你可能需要检查你的网络连接',
+          },
+          footer: {
+            selectText: '选择',
+            navigateText: '切换',
+            closeText: '关闭',
+            searchByText: '搜索提供者',
+          },
+          noResultsScreen: {
+            noResultsText: '无法找到相关结果',
+            suggestedQueryText: '你可以尝试查询',
+            reportMissingResultsText: '你认为该查询应该有结果？',
+            reportMissingResultsLinkText: '点击反馈',
+          },
+        },
+      },
+    })
+  ],
 
   shouldPrefetch: false,
 })
